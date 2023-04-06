d262dc6c04cc8ed8c4402e7546860670829938a5
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2023-04-06 17:06:37 -0500

    added

---
+++ workout/my-react-app/src/To.js
@@ -0,0 +1,24 @@
+import React from "react";
+export function TO(){
+  return(
+<div>
+  <div class="conatiner">
+    <div class="row">
+      <div class="col-2">
+    <a href="">wednesday</a>
+    <div class="row">
+      <div class="col-1">
+        <h4>warmup</h4>
+        <ul>
+          <li>
+            <a href="https://youtu.be/Bw88PXaGUxU">first</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </div>
+  </div>
+</div>
+  );  
+}
